1d02c4a544574bf660f39b1a171f9237423176d1
Kevin Buchholz <buchholz.kevin@gmail.com>
2023-10-26 17:00:41 -0400

    Initial Commit

---
+++ RandomCurseGenerator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RandomCurseGenerator
+//
+//  Created by Kevin Buchholz on 10/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RandomCurseGenerator/RandomCurseGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomCurseGeneratorApp.swift
+//  RandomCurseGenerator
+//
+//  Created by Kevin Buchholz on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomCurseGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
